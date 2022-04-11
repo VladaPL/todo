69c0dc6e7c1fb6eae0592f06cc41a23e950c5d5a
Vlada Plutalova <vladaplutalova@yandex.ru>
2022-04-11 16:23:09 +0300

    feat: add items from array

--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -1,11 +1,19 @@
 import React from "react";
 import TodoListItem from "./todo-list-item";
 
-const TodoList = () => {
+const TodoList = ({ todos }) => {
+  const elements = todos.map((item) => {
+    const { id, ... itemProps } = item;
+    return (
+      <li key={ id }>
+        <TodoListItem { ... itemProps } /> {/* взять все свойства из объекта item и передать их в качестве атрибута вместе со значением */}
+      </li>
+    );
+  });
+
   return (
     <ul>
-      <li><TodoListItem label="Drink coffee"/></li>
-      <li><TodoListItem label="Build React app"/></li>
+      { elements }
     </ul>
   );
 };
--- src/index.js
+++ src/index.js
@@ -6,11 +6,17 @@ import TodoList  from "./components/todo-list";
 import "./index.css";
 
 function App() {
+  const todoData = [
+    { label: "create layout", important: false, id: 1},
+    { label: "drink coffee", important: true, id : 2},
+    { label: "fix geneus-trees", important: false, id: 3},
+  ];
+
   return (
     <div>
       <Header />
       <SearchPanel />
-      <TodoList/>
+      <TodoList todos={todoData} />
     </div>
   );
 }
