819af2f56659afd053bbb636c5e8acca19987fc2
Vlada Plutalova <vladaplutalova@yandex.ru>
2022-04-11 13:46:45 +0300

    feat: add components - header - search panel - todo-list

---
+++ src/components/app-header.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Header = () => {
+  return <h1>ToDo list</h1>;
+};
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Header from "./components/app-header";
+import  SearchPanel  from "./components/search-panel";
+import TodoList  from "./components/todo-list";
 import "./index.css";
 
 function App() {
-  return <div className="App">Hello! It is ToDo list!</div>;
+  return (
+    <div>
+      <Header />
+      <SearchPanel />
+      <TodoList/>
+    </div>
+  );
 }
 
 ReactDOM.render(<App />, document.getElementById("root"));
