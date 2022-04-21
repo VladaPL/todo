4e4f9d10b7d3cf521106bc7a87c4d54c623cbffd
Vlada Plutalova <vladaplutalova@yandex.ru>
2022-04-21 16:08:46 +0300

    refactor: - add new folder app - add class for ItemStatusFilter and TodoListItem

--- src/index.js
+++ src/index.js
@@ -1,41 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import AppHeader from './components/app-header/app-header';
-import  SearchPanel  from "./components/search-panel/search-panel";
-import TodoList  from "./components/todo-list/todo-list";
-import ItemStatusFilter from './components/item-status-filter/item-status-filter';
-import "./index.css";
 
-function App() {
-  const todoData = [
-    { 
-      label: "create layout",
-      important: false,
-      id: 1
-    },
-    { 
-      label: "drink coffee",
-      important: true,
-      id : 2
-    },
-    { 
-      label: "fix geneus-trees",
-      important: false,
-      id: 3
-    },
-  ];
-
-  return (
-    <div className="todo-app">
-      <AppHeader toDo={1} done={3} />
-      <div className="top-panel d-flex">
-        <SearchPanel />
-        <ItemStatusFilter />
-      </div>
-
-      <TodoList todos={todoData} />
-    </div>
-  );
-}
+import App from './components/app';
 
 ReactDOM.render(<App />, document.getElementById("root"));
