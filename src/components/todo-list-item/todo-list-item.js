87fb055c25b73adc94578bd81c7a951902a99337
Vlada Plutalova <vladaplutalova@yandex.ru>
2022-04-29 14:24:17 +0300

    feat: add image and colors

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -13,23 +13,25 @@ export default class TodoListItem extends Component {
     };
   
     return (
-      <span className="todo-list-item">
+      <div className="todo-list-item">
         <span
           className="todo-list-item-label"
           style={style}>
           {label}
         </span>
-  
-        <button type="button"
-                className="btn btn-outline-success btn-sm float-right">
-          X
-        </button>
-  
-        <button type="button"
-                className="btn btn-outline-danger btn-sm float-right">
-          <i className="fa fa-trash-o" />!
-        </button>
-      </span>
+        <span className='wrapper-button'>
+          <button type="button"
+                  className="btn btn-outline-success btn-sm float-right">
+            X
+          </button>
+    
+          <button type="button"
+                  className="btn btn-outline-danger btn-sm float-right">
+            <i className="fa fa-trash-o" />!
+          </button>
+        </span>
+        
+      </div>
     );
   };
 }
