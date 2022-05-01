1a4ab49956fc828faf2b6635b3ebfb85c028d282
Vlada Plutalova <vladaplutalova@yandex.ru>
2022-05-01 13:14:10 +0300

    feat: add function setState for important and done items

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -4,33 +4,59 @@ import './todo-list-item.css';
 
 export default class TodoListItem extends Component {
 
-  render () {
-    const { label, important = false } = this.props;
+  constructor() {
+    super();
 
-    const style = {
-      color: important ? 'steelblue' : 'black',
-      fontWeight: important ? 'bold' : 'normal'
+    this.state = {
+      done: false,
+      important: false
     };
+
+    this.onLableClick = () => {
+      this.setState({
+        done: true
+      });
+    };
+
+    this.onMarkImportant = () => {
+      this.setState({
+        important: true
+      });
+    };
+  }
+
+  render () {
+    const { label } = this.props;
+    const { done, important } = this.state;
+
+    let classNames = 'todo-list-item';
+    if(done) {
+      classNames += ' done';
+    }
+
+    if(important) {
+      classNames += ' important';
+    }
   
     return (
-      <div className="todo-list-item">
+      <div className={classNames}>
         <span
           className="todo-list-item-label"
-          style={style}>
+          onClick={this.onLableClick}>
           {label}
         </span>
         <span className='wrapper-button'>
           <button type="button"
-                  className="btn btn-outline-success btn-sm float-right">
-            X
+                  className="btn btn-outline-success btn-sm float-right"
+                  onClick = {this.onMarkImportant}>
+            <i className="fa fa-trash-o" />!
           </button>
     
           <button type="button"
                   className="btn btn-outline-danger btn-sm float-right">
-            <i className="fa fa-trash-o" />!
+            <i className="fa fa-exclamation" />
           </button>
         </span>
-        
       </div>
     );
   };
