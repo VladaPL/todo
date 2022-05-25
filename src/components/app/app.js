0a041137bad9dec6c8a0102f2ab6bcf9cc15dba7
Vlada Plutalova <vladaplutalova@yandex.ru>
2022-05-25 11:40:37 +0300

    feat: add func toggleProperty

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -56,41 +56,47 @@ export default class App extends Component {
     });
   };
 
+  toggleProperty(arr, id, propName) {
+    const index = arr.findIndex((el) => el.id === id);
+    // 1. update object
+    const oldItem = arr[index];
+    const newItem = { ...oldItem, [propName]: !oldItem[propName] }; // создали копию oldItem и перезаписали свойство done на противоположное
+
+    // 2. construct new array
+    return [...arr.slice(0, index), newItem, ...arr.slice(index + 1)];
+  }
+
   onToggleImportant = (id) => {
-    console.log("Toggle important", id);
+    this.setState(({ todoData }) => {
+      return {
+        todoData: this.toggleProperty(todoData, id, 'important')
+      };
+    });
   };
 
   onToggleDone = (id) => {
     this.setState(({ todoData }) => {
-      const index = todoData.findIndex((el) => el.id === id);
-      // 1. update object
-      const oldItem = todoData[index];
-      const newItem = { ...oldItem, done: !oldItem.done }; // создали копию oldItem и перезаписали свойство done на противоположное
-
-      // 2. construct new array
-      const newArray = [
-        ...todoData.slice(0, index),
-        newItem,
-        ...todoData.slice(index + 1),
-      ];
-
       return {
-        todoData: newArray,
+        todoData: this.toggleProperty(todoData, id, 'done')
       };
     });
   };
 
   render() {
+    const { todoData } = this.state;
+    const doneCount = todoData.filter((el) => el.done).length;
+    const todoCount = todoData.length - doneCount;
+
     return (
       <div className="todo-app">
-        <AppHeader toDo={1} done={3} />
+        <AppHeader toDo={todoCount} done={doneCount} />
         <div className="top-panel d-flex">
           <SearchPanel />
           <ItemStatusFilter />
         </div>
 
         <TodoList
-          todos={this.state.todoData}
+          todos={todoData}
           onDeleted={this.deleteItem}
           onToggleImportant={this.onToggleImportant}
           onToggleDone={this.onToggleDone}
